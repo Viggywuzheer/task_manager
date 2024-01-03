dce6bfc3e16ce965b6ccf793faeed5f4e85a2eb5
Vignesh Senguttuvan <vigneshsenguttuvan@gmail.com>
2024-01-02 21:47:59 -0500

    updates urls for new views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
         path('',TaskListView.as_view(), name='task-list'),
+        path('add/', TaskCreateView.as_view(), name = 'task-add'),
+        path('<int:pk>/edit/', TaskUpdateView.as_view(), name = 'task-edit'),
 ]
