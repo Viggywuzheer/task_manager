f37089e0e9d0266131fa27131784b9ff44b078a9
Vignesh Senguttuvan <vigneshsenguttuvan@gmail.com>
2024-01-02 21:16:04 -0500

    updates 'view.py' for adding and updating tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Task
+from .forms import TaskCreateForm, TaskUpdateForm
 
 # Create your views here.
 
@@ -9,4 +11,14 @@ class TaskListView(ListView):
     template_name = 'task_list.html'
     context_object_name = 'tasks'
 
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskCreateForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
 
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskUpdateForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
